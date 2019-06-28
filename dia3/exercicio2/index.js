a05f6ac4909a043e4dec589dbd98d2cc1c09098b
Luiz Ramos <luizgdramos@gmail.com>
2019-06-28 20:34:35 -0300

    Resolvendo exercícios do dia 3 - Foguete

---
+++ dia3/exercicio2/index.js
@@ -0,0 +1,30 @@
+let input = document.getElementById("count");
+let button = document.getElementById("btnCount");
+let label = document.getElementById("contagemRegressiva");
+let rocket = document.getElementsByClassName("imagem-rocket")[0];
+
+button.addEventListener("click", function() {
+    label.innerText = input.value;
+    var seconds = input.value;
+    var timer = setInterval(function() {
+        seconds--;
+        label.innerText = seconds;
+        if (seconds <= 0) {
+            sendRocket();
+            clearInterval(timer);
+        };
+    }, 1000);
+});
+
+function sendRocket() {
+    var position = 0;
+    var timer = setInterval(function() {
+        if (position == 500 - 145) {
+            clearInterval(timer);
+            alert("Lançamento concluído")
+        } else {
+            position++;
+            rocket.style["margin-bottom"] = position + "px";
+        }
+    }, 10);
+}
